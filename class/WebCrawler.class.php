2762a6f11e69a86d553e47b778e09141b9d21541
Karim Ainine <karim.ainine@gmail.com>
2012-05-05 01:56:21 +1000

    Revert "Training…"
    This reverts commit 179c26666757b712701b28dd0ab7d159c06b90f4.

--- class/WebCrawler.class.php
+++ class/WebCrawler.class.php
@@ -14,7 +14,7 @@ require_once 'WebPage.class.php';
 require_once 'inc/simple_html_dom.php';
 
 // Crawler constants
-define("MAX_PAGES", 2);
+define("MAX_PAGES", 10);
 define("DEFAULT_SEED", "http://www.theage.com.au/digital-life/mobiles");
 define("DEFAULT_RELEVANT_SEED", "http://www.theage.com.au/digital-life/mobiles");
 define("DEFAULT_IRRELEVANT_SEED", "http://www.theage.com.au");
@@ -42,7 +42,6 @@ class WebCrawler {
     private $pages_counter;
     private $all_keywords = array();
     private $train;
-    private $counter;
 
     // Main WebCrawler Class constructor
     function __construct($politeness = DEFAULT_POLITENESS, $maxpages = MAX_PAGES, $seed_url = DEFAULT_SEED, $train = true, $all_keywords = array()) {
@@ -50,19 +49,14 @@ class WebCrawler {
         $this->politeness = $politeness;
         $this->maxpages = $maxpages;
         $this->seed_url = $seed_url;
+        $this->host = $this->getHost();
         $this->pages_counter = 0;
-        $this->counter = 0;
-        if (!$this->train) {
+        if (!$this->train)
             $this->all_keywords = $all_keywords;
-            $this->host = $this->getHost();
-        }else{
-            $this->seed_url = DEFAULT_RELEVANT_SEED;
-            $this->seed_url = DEFAULT_RELEVANT_SEED;
-        }
     }
 
     public function start() {
-        //self::makeDataCleanUp();
+        self::makeDataCleanUp();
         $this->start_time = time();
         $this->crawl_dfs($this->seed_url);
         foreach ($this->visitedPages as $article) {
@@ -88,18 +82,14 @@ class WebCrawler {
             }
 
             if ($this->train) {
-                print_r("\n".($this->counter)."\n");
-                if (($this->counter) == (MAX_PAGES / 2)) {
-                    print_r("\nswitch hosts\n");
-                    $this->seed_url = DEFAULT_IRRELEVANT_SEED;
-                    $this->host = DEFAULT_IRRELEVANT_SEED;
-                    $this->start();
+                if (count($this->visitedPages) <= (MAX_PAGES / 2)) {
+                    $page = new WebPage($url, DEFAULT_RELEVANT_SEED);
+                }else{
+                    $page = new WebPage($url, DEFAULT_IRRELEVANT_SEED);
                 }
-                $this->counter++;
+            } else {
+                $page = new WebPage($url, $this->host);
             }
-            
-            $page = new WebPage($url, $this->host);
-            
             $this->visitedPages[] = $page;
             $this->visitedLinks[] = $url;
             $this->pages_counter++;
